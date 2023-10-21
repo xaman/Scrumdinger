65dcaedb3e12605cb6db62218d765d5dbfdbd7e7
Martin Chamarro <martin.chamarro@gmail.com>
2023-10-21 16:40:44 +0100

    Implement the logic to sync the state between views

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct DetailEditView: View {
-    @State private var scrum = DailyScrum.emptyScrum
+    @Binding var scrum: DailyScrum
     @State private var newAttendeeName = ""
 
     var body: some View {
@@ -23,6 +23,7 @@ struct DetailEditView: View {
                     Spacer()
                     Text("\(scrum.lengthInMinutes) minutes")
                 }
+                ThemePicker(selection: $scrum.theme)
             }
             Section(header: Text("Attendees")) {
                 ForEach(scrum.attendees) { attendee in
@@ -52,6 +53,6 @@ struct DetailEditView: View {
 
 struct DetailEditView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailEditView()
+        DetailEditView(scrum: .constant(DailyScrum.sampleData[0]))
     }
 }
---
+++ Scrumdinger/ThemePicker.swift
@@ -0,0 +1,26 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by Martin Chamarro on 21/10/2023.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+
+    var body: some View {
+        Picker("Theme", selection: $selection) {
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+        .pickerStyle(.navigationLink)
+    }
+}
+
+#Preview {
+    ThemePicker(selection: .constant(.periwinkle))
+}
