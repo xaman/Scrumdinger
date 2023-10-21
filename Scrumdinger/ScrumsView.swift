edeed124808811195a8cc27043d4525705a341eb
Martin Chamarro <martin.chamarro@gmail.com>
2023-10-21 13:34:23 +0100

    Create the UI for the list of meetings

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,24 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Martin Chamarro on 21/10/2023.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
